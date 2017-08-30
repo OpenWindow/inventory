bb3b97a27b28ab80b350fe94f8f4ff5ed6702060
Neel <neelavardhanreddym@gmail.com>
2017-08-30 00:35:49 -0400

    Add InMemoryDatabase library for using in DbContext

--- Inventory.Api/Startup.cs
+++ Inventory.Api/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Inventory.Business.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Inventory.Api
 {
@@ -16,6 +18,7 @@ namespace Inventory.Api
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<InventoryContext>( opt => opt.UseInMemoryDatabase("Inventory"));
             services.AddWebApi();
         }
 
