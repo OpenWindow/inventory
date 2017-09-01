57e6c12078e888de7d91f5f944ad3468078e89bf
Neel <neelavardhanreddym@gmail.com>
2017-08-31 21:01:23 -0400

    Add simple api controller for basic working of the api.

---
+++ Inventory.Api/Controllers/InventoryItemsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inventory.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class InventoryItemsController : ControllerBase
+    {
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            return Ok(new { Name = "Neel", FavoriteColor = "Blue" });
+        }
+
+
+    }
+}
