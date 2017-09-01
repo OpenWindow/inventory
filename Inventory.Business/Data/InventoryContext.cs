a3e061fa4d169475a6f0ed72ebd14e8f26ff5158
Neel <neelavardhanreddym@gmail.com>
2017-08-31 22:01:03 -0400

    Initial model classes and dbcontext

---
+++ Inventory.Business/Data/InventoryContext.cs
@@ -0,0 +1,29 @@
+﻿using Inventory.Business.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Inventory.Business.Data
+{
+    public class InventoryContext : DbContext
+    {
+        public string ConnectionString { get; set; }
+
+        public InventoryContext(DbContextOptions<InventoryContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<InventoryCategory> InventoryCategory { get; set; }
+        public DbSet<InventoryItem> InventoryItems { get; set; }
+        public DbSet<ShoppingListItem> ShoppingListItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            // overrider the table names created from DbSet Property names
+            builder.Entity<InventoryCategory>().ToTable("InventoryCategory");
+            builder.Entity<InventoryItem>().ToTable("InventoryItem");
+            builder.Entity<ShoppingListItem>().ToTable("ShoppingList");
+
+        }
+
+    }
+}
--- Inventory.Business/Models/InventoryEntities.cs
+++ Inventory.Business/Models/InventoryEntities.cs
@@ -4,13 +4,45 @@ using System.Text;
 
 namespace Inventory.Business.Models
 {
+    public class InventoryCategory
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public ICollection<InventoryItem> InventoryItems { get; set; }
+
+        public byte[] RowVersion { get; set; }
+    }
+
     public class InventoryItem
     {
-        public int Id { get; set; }
+        public int InventoryItemID { get; set; }
+        public int InventoryCategoryID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public double Price { get; set; }
         public DateTime PurchaseDate { get; set; }
         public string PreviewImageUrl { get; set; }
+
+        public InventoryCategory InventoryCategory { get; set; }
+
+        public byte[] RowVersion { get; set; }
+    }
+
+    public enum UnitOfMeasurement
+    {
+        none, lbs, gallons, oz, kgs, @case, unit
+    }
+
+    public class ShoppingListItem
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public UnitOfMeasurement? UnitOfMeasurement { get; set; }
+        public string Description { get; set; }
+
+        public byte[] RowVersion { get; set; }
     }
 }
