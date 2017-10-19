54ac48709ff6fe2a209a5183aeb716f64f416086
Neel <neelavardhanreddym@gmail.com>
2017-10-18 23:35:13 -0400

    Fix rename of the class

--- Inventory.Data/DbInitializer.cs
+++ Inventory.Data/DbInitializer.cs
@@ -1,4 +1,4 @@
-﻿using Inventory.Data.Models;
+﻿using Inventory.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,75 +13,75 @@ namespace Inventory.Data
             context.Database.EnsureCreated();
 
             // Look for any inventory items
-            if (context.InventoryCategories.Any())
+            if (context.Categories.Any())
             {
                 return; // DB has been seeded
             }
 
-            var categories = new InventoryCategory[]
+            var categories = new Category[]
             {
-                new InventoryCategory { Name ="Kitchen", Description="all inventory in kitchen" },
-                new InventoryCategory { Name ="Living Room", Description="all inventory in living room" },
-                new InventoryCategory { Name ="Bed Rooms", Description="all inventory in all bed rooms" }
+                new Category { Name ="Kitchen", Description="all inventory in kitchen" },
+                new Category { Name ="Living Room", Description="all inventory in living room" },
+                new Category { Name ="Bed Rooms", Description="all inventory in all bed rooms" }
             };
-            foreach (InventoryCategory c in categories)
+            foreach (Category c in categories)
             {
-                context.InventoryCategories.Add(c);
+                context.Categories.Add(c);
             }
             context.SaveChanges();
 
             var inventoryItems = new InventoryItem[]
             {
                 new InventoryItem {
-                    InventoryCategoryID = 1,
+                    CategoryID = 1,
                     Name ="Wet Grinder",
                     Price =200.00,
                     PreviewImageUrl ="http://via.placeholder.com/150x150/ffffff/000000?text=wet+grinder",
                     PurchaseDate = DateTime.Parse("2005-09-01")},
                 new InventoryItem {
-                    InventoryCategoryID = 1,
+                    CategoryID = 1,
                     Name ="Dining Table",
                     Price =100.00,
                     PreviewImageUrl ="http://via.placeholder.com/150x150/ffffff/000000?text=Dining+Table",
                     PurchaseDate = DateTime.Parse("2005-09-01")},
                 new InventoryItem {
-                    InventoryCategoryID = 1,
+                    CategoryID = 1,
                     Name ="Rice Cooker",
                     Price =60.00,
                     PreviewImageUrl ="http://via.placeholder.com/150x150/ffffff/000000?text=rice+cooker",
                     PurchaseDate = DateTime.Parse("2005-09-01")},
                 new InventoryItem {
-                    InventoryCategoryID = 2,
+                    CategoryID = 2,
                     Name ="Sofa",
                     Price =600.00,
                     PreviewImageUrl ="http://via.placeholder.com/150x150/ffffff/000000?text=sofa",
                     PurchaseDate = DateTime.Parse("2005-09-01")},
                 new InventoryItem {
-                    InventoryCategoryID = 2,
+                    CategoryID = 2,
                     Name ="Samsung TV",
                     Price =1000.00,
                     PreviewImageUrl ="http://via.placeholder.com/150x150/ffffff/000000?text=Samsung+TV",
                     PurchaseDate = DateTime.Parse("2005-09-01")},
                 new InventoryItem {
-                    InventoryCategoryID = 2,
+                    CategoryID = 2,
                     Name ="X-Arcade",
                     Price =160.00,
                     PreviewImageUrl ="http://via.placeholder.com/150x150/ffffff/000000?text=X-Arcade",
                     PurchaseDate = DateTime.Parse("2005-09-01")},
                 new InventoryItem {
-                    InventoryCategoryID = 3,
+                    CategoryID = 3,
                     Name ="Mattress",
                     Price =500.00,
                     PreviewImageUrl ="http://via.placeholder.com/150x150/ffffff/000000?text=mattress",
                     PurchaseDate = DateTime.Parse("2005-09-01")},
                 new InventoryItem {
-                    InventoryCategoryID = 3,
+                    CategoryID = 3,
                     Name ="Dressing Table",
                     Price =300.00,
                     PreviewImageUrl ="http://via.placeholder.com/150x150/ffffff/000000?text=Dressing+Table",
                     PurchaseDate = DateTime.Parse("2005-09-01")},
                 new InventoryItem {
-                    InventoryCategoryID = 3,
+                    CategoryID = 3,
                     Name ="Bed",
                     Price =200.00,
                     PreviewImageUrl ="http://via.placeholder.com/150x150/ffffff/000000?text=bed",
