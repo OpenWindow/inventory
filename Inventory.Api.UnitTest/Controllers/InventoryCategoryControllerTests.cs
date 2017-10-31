0da837b100188d912848911d5eedf3cb80be5672
Neel <neelavardhanreddym@gmail.com>
2017-10-30 23:10:28 -0400

    added unit test project

---
+++ Inventory.Api.UnitTest/Controllers/InventoryCategoryControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using Inventory.Api.Controllers;
+using Inventory.Api.Features.Categories;
+using Inventory.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Inventory.Api.UnitTest.Controllers
+{
+
+    public class InventoryCategoryControllerTests
+    {
+        [Fact]
+        public void UnitTest1()
+        {
+          
+        }
+
+    }
+}
